f06c14bb101d14a9f0a310bb590cc272ad9c399b
Dipto_008 <142109281+dipto-008@users.noreply.github.com>
2024-10-29 23:21:29 +0000

    Up

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const { handleMessage } = require('./main.js');
-const config = require('./config.json');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const { handleMessage } = require("./main.js");
+const config = require("./config.json");
 
 const app = express();
 app.use(bodyParser.json());
 
-// Serve the landing page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Webhook for Messenger
-app.get('/webhook', (req, res) => {
-    const VERIFY_TOKEN = 'dipto008';
-    const { mode, token, challenge } = req.query;
+app.get("/webhook", (req, res) => {
+    const VERIFY_TOKEN = "dipto008";
 
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-        console.log('WEBHOOK_VERIFIED');
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+        console.log("WEBHOOK_VERIFIED");
         res.status(200).send(challenge);
     } else {
         res.sendStatus(403);
     }
 });
 
-app.post('/webhook', async (req, res) => {
+app.post("/webhook", async (req, res) => {
     const body = req.body;
     //console.log(body)
-    if (body.object === 'page') {
+    if (body.object === "page") {
         for (const entry of body.entry) {
             const webhook_event = entry.messaging[0];
             const sender_psid = webhook_event.sender.id;
-
-         //   console.log("webhook_event", webhook_event);
 
             if (webhook_event.message) {
                 await handleMessage(sender_psid, webhook_event.message);
             }
         }
-        res.status(200).send('EVENT_RECEIVED');
+        res.status(200).send("EVENT_RECEIVED");
     } else {
         res.sendStatus(404);
     }
 });
 
-// Start the server
-app.listen(1337, () => console.log('Server is running on port 1337'));
+
+app.listen(1337, () => console.log("Server is running on port 1337"));
