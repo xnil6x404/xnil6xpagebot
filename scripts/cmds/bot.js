4e19e6b0cee34e7572b3abcfaad47c65f08fd49d
Badboy-404 <108590422+badboysabbir8@users.noreply.github.com>
2024-11-02 13:20:25 +0600

    Create bot.js

---
+++ scripts/cmds/bot.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+
+module.exports.config = {
+    name: "bot",
+    aliases: ["bby"],
+    version: "1.0",
+    credits: "Dipto",
+    role: 3,
+    usePrefix: false,
+    description: "Chat with baby",
+    commandCategory: "fun",
+    guide: "{prefix}baby <message>",
+    coolDowns: 5,
+};
+
+module.exports.onStart = async ({ event, args, message }) => {
+    const msg = args.join(" ");
+
+    if (!msg) {
+        return message.reply("Please provide a message.");
+    }
+
+    try {
+        const apiUrl = `https://www.noobs-api.000.pe/dipto/baby?text=${encodeURIComponent(msg)}`;
+        const response = await axios.get(apiUrl);
+        const data = response.data.reply;
+
+        await message.reply(data);
+    } catch (error) {
+        message.reply(`Error: ${error.message}`);
+    }
+};
